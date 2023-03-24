640182803b8c5983a567128ec75e3aa98c402094
Suraj Gopal <surajcancode@gmail.com>
2023-03-24 11:39:19 -0400

    todo list model created, migrated, controller created, ++testfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'tdlists/index'
+  get 'tdlists/create'
+  get 'tdlists/update'
+  get 'tdlists/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
